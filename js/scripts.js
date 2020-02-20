dda4f98de561a14935cb63c790b11250d9af0049
Winter Runion <winterrunion@gmail.com>
2020-02-19 18:30:18 -0800

    Create separate functions to confirm and display order information to user.

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,13 @@ class Pizza {
       this.name = customerName;
       this.size = size;
       this.toppings = toppings;
-      //NOTE: Should I have price as a property that is a function? Or as a separate method, like below?
-      // this.price = function() {
-      //   let total = 0;
-      //   //Set base price of pizza based on size
-      //   if (this.size === "small") { total += 10; }
-      //   if (this.size === "medium") { total += 15; }
-      //   if (this.size === "large") { total += 20; }
-      //   //Check to see if user ordered extra toppings. If so, charge them $1 extra per topping.
-      //   if (this.toppings) { total += this.toppings.length; }
-      //   return total;
-      // }
+      // ***QUESTION FOR MEGAN*** 
+      // Is it better practice to have price as a property that is a function?
+      // For example: 
+          //this.price = function() {
+            // (calculate and return price here);
+          // }
+      // Is that more effective? Or is it better to do it as a separate method, like I did below?
   }
   getPrice() {
         let total = 0;
@@ -32,37 +28,77 @@ class Pizza {
     } 
     return `You ordered a ${this.size} pizza with ${this.toppings.length} extra toppings. Is that correct?`;
   }    
+  submitOrder() {
+    return `Your total is $${this.getPrice()}. Press "okay" to submit your order.`;
+  }
+  thankCustomer() {
+    return `Thank you for your order, ${this.name}. Your pizza will be ready in 30 minutes or less.`;
+  }
   // displayOrder() {
-  //   return [`Name: ${this.name}`, ]
   //   //return the customer's info in a nice, easy to display format
   // }
 }
 
+// Function to display the customer's order back to them and ask for confirmation 
+function confirmOrder(size, toppings=[]) {
+  if (toppings.length === 1) {
+    return `You would like a ${size} pizza with ${toppings.length} additional topping. Is that correct?`; 
+  } else {
+  return `You would like a ${size} pizza with ${toppings.length} additional toppings. Is that correct?`;
+  }
+}
+
+function thankCustomer(customerName){
+  return `Thank you for your order, ${customerName}! It will be ready for pickup in 30 minutes or less!`;
+}
 
 // UI Logic --------------------------
 $(document).ready(function() {
-  // console.log('jquery is working');
   $("form#orderForm").submit(function(event) {
-    // console.log('form has been submitted');
     let name = $("input#name").val();
     let size = $("input:radio[name=size]:checked").val();
     let toppings = [];
     $.each($("input[name='topping']:checked"), function(){
         toppings.push($(this).val());
-   });
-    // console.log(toppings);
-    // console.log(typeof toppings);
-    // console.log(name);
-    // console.log(size);
-   let pizza = new Pizza(name, size, toppings);
-   console.log(pizza);
-   console.log(pizza.getPrice());
-   confirm(pizza.confirmOrder());
-  //  console.log(pizza.price());
-    // $("#results-div").append(name, size, toppings);
+    });
+    //Confirm the order with the user
+    let confirmSelections = confirm(confirmOrder(size, toppings));
+    
+    // Create a new instance of Pizza using that information
+    if (confirmSelections) {
+      let pizza = new Pizza(name, size, toppings);
+      console.log(pizza);
+      console.log(pizza.getPrice());
+      // Display and confirm total price with user
+      let submitOrder = confirm(`Your total is $${pizza.getPrice()}. Press "okay" to submit your order.`);
+
+      if (submitOrder) {
+      alert(thankCustomer(name));
+    }
+
+    }
+
+ 
+    // Run the "price" method on our pizza instance
+
+
+    // let price = currentPizza.totalPrice();
+
+    // // Display that price total to the user
+    // confirm(`Your total is $${price}. Please press "OK" to submit your order`);
+    // // Thank the user for their order.
+    // alert(`Thanks for your order, ${userName}. It will be ready for pickup in 30 minutes or less.`);
+
+
+
+
+    // let pizza = new Pizza(name, size, toppings);
+    // confirm(pizza.confirmOrder());
+    // confirm(pizza.submitOrder());
+    // alert(pizza.thankCustomer());
+
     event.preventDefault();
   });
-
 });
 
 
